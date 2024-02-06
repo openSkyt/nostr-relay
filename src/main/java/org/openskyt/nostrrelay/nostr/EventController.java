76a9a850adcc6d8c226ff845f9eef7a0e6d086a3
PavelSlimBilek <bilekpavel.p@gmail.com>
2024-02-06 17:51:08 +0100

    DB - presence check before saving

---
+++ src/main/java/org/openskyt/nostrrelay/nostr/EventController.java
@@ -0,0 +1,64 @@
+package org.openskyt.nostrrelay.nostr;
+
+import org.openskyt.nostrrelay.model.Event;
+import org.openskyt.nostrrelay.model.NostrConsumer;
+import org.openskyt.nostrrelay.observers.EventObserver;
+import org.springframework.stereotype.Component;
+
+import java.util.Optional;
+
+/**
+ * Handles EVENT-data processing logic
+ */
+@Component
+public class EventService implements NostrConsumer {
+    private final NostrPersistence persistence;
+
+    public EventService(EventObserver observer, NostrPersistence persistence) {
+        this.persistence = persistence;
+
+        observer.subscribe(this);
+    }
+
+    /**
+     * Handles EVENT by delegating EVENT-data to a proper logic-handling method defined by EVENT-kind
+     * @param event
+     * parsed EVENT-message data
+     */
+    private void handle(Event event) {
+        switch (event.getKind()) {
+            case 0      : handleEvent_0(event); break;
+            case 1      : handleEvent_1(event); break;
+            default     : System.out.println("ignoring event: " + event);
+        }
+    }
+
+    /**
+     * Handles EVENT-kind 0 by checking redundancy, saves to DB. Responds with OK-message
+     * @param event
+     * incoming EVENT-data
+     */
+    private void handleEvent_0(Event event) {
+        Optional<Event> optEventData = persistence.getMetaData(event.getPubkey());
+        // remove redundant data
+        optEventData.ifPresent(persistence::delete);
+        persistence.save(event);
+    }
+
+    /**
+     * Handles EVENT-kind 1 by saving into the DB. Responds with OK-message.
+     * @param event
+     * incoming EVENT-data
+     */
+    private void handleEvent_1(Event event) {
+        persistence.save(event);
+    }
+
+    // overridden method from implemented NostrConsumer interface - invokes actual impl. defined in this class
+    @Override
+    public void handle(Object o) {
+        if (o instanceof Event) {
+            handle((Event) o);
+        }
+    }
+}
--- src/main/java/org/openskyt/nostrrelay/nostr/EventService.java
+++ src/main/java/org/openskyt/nostrrelay/nostr/EventService.java
@@ -1,64 +1,119 @@
 package org.openskyt.nostrrelay.nostr;
 
+import lombok.RequiredArgsConstructor;
+import org.openskyt.nostrrelay.dto.ReqFilter;
 import org.openskyt.nostrrelay.model.Event;
-import org.openskyt.nostrrelay.model.NostrConsumer;
-import org.openskyt.nostrrelay.observers.EventObserver;
+import org.openskyt.nostrrelay.repository.EventRepository;
+import org.springframework.data.mongodb.core.MongoTemplate;
+import org.springframework.data.mongodb.core.query.Criteria;
+import org.springframework.data.mongodb.core.query.Query;
+import org.springframework.scheduling.annotation.EnableScheduling;
+import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
 
+import java.util.HashSet;
+import java.util.List;
 import java.util.Optional;
+import java.util.Set;
 
-/**
- * Handles EVENT-data processing logic
- */
 @Component
-public class EventService implements NostrConsumer {
-    private final NostrPersistence persistence;
+@RequiredArgsConstructor
+@EnableScheduling
+public class NostrPersistence {
 
-    public EventService(EventObserver observer, NostrPersistence persistence) {
-        this.persistence = persistence;
+    private final EventRepository repo;
+    private final MongoTemplate mongoTemplate;
 
-        observer.subscribe(this);
+    public void save(Event event) {
+        repo.save(event);
     }
 
-    /**
-     * Handles EVENT by delegating EVENT-data to a proper logic-handling method defined by EVENT-kind
-     * @param event
-     * parsed EVENT-message data
-     */
-    private void handle(Event event) {
-        switch (event.getKind()) {
-            case 0      : handleEvent_0(event); break;
-            case 1      : handleEvent_1(event); break;
-            default     : System.out.println("ignoring event: " + event);
+    public void delete(Event event) {
+        try {
+            repo.deleteById(event.getId());
+        } catch (Exception e) {
+            throw new RuntimeException();
         }
     }
 
-    /**
-     * Handles EVENT-kind 0 by checking redundancy, saves to DB. Responds with OK-message
-     * @param event
-     * incoming EVENT-data
-     */
-    private void handleEvent_0(Event event) {
-        Optional<Event> optEventData = persistence.getMetaData(event.getPubkey());
-        // remove redundant data
-        optEventData.ifPresent(persistence::delete);
-        persistence.save(event);
+    public Optional<Event> getMetaData(String pubkey) {
+        return repo.findByPubkeyAndByKind(pubkey, 0).stream().findAny();
     }
 
     /**
-     * Handles EVENT-kind 1 by saving into the DB. Responds with OK-message.
-     * @param event
-     * incoming EVENT-data
+     * Retrieves requested data based on request filter sent by client in REQ-message
+     * @param reqFilterSet
+     * subscription filter-set
+     * @return
+     * valid persisted event data
      */
-    private void handleEvent_1(Event event) {
-        persistence.save(event);
+    public Set<Event> getAllEvents(Set<ReqFilter> reqFilterSet) {
+        Set<Event> events = new HashSet<>();
+
+        reqFilterSet.forEach(request -> {
+            Criteria criteria = new Criteria();
+
+            if (request.getAuthors() != null && !request.getAuthors().isEmpty()) {
+                criteria.and("pubkey").in(request.getAuthors());
+            }
+
+            if (request.getKinds() != null && !request.getKinds().isEmpty()) {
+                criteria.and("kind").in(request.getKinds());
+            }
+
+            if (request.getIds() != null && !request.getIds().isEmpty()) {
+                criteria.and("id").in(request.getIds());
+            }
+
+            if (request.getE() != null && !request.getE().isEmpty()) {
+                criteria.and("tags").elemMatch(
+                        Criteria.where("0").is("e").and("1").in(request.getE())
+                );
+            }
+            if (request.getP() != null && !request.getP().isEmpty()) {
+                criteria.and("tags").elemMatch(
+                        Criteria.where("0").is("p").and("1").in(request.getP())
+                );
+            }
+            if (request.getT() != null && !request.getT().isEmpty()) {
+                criteria.and("tags").elemMatch(
+                        Criteria.where("0").is("t").and("1").in(request.getT())
+                );
+            }
+
+            if (request.getSince() != null) {
+                criteria.and("created_at").gte(request.getSince());
+            }
+            if (request.getUntil() != null) {
+                criteria.and("created_at").lte(request.getUntil());
+            }
+
+            if (request.getLimit() != null) {
+                Query limitedQuery = new Query(criteria).limit(request.getLimit());
+                events.addAll(mongoTemplate.find(limitedQuery, Event.class));
+            } else {
+                Query query = new Query(criteria);
+                events.addAll(mongoTemplate.find(query, Event.class));
+            }
+        });
+        return events;
     }
 
-    // overridden method from implemented NostrConsumer interface - invokes actual impl. defined in this class
-    @Override
-    public void handle(Object o) {
-        if (o instanceof Event) {
-            handle((Event) o);
+    // TODO - přesunout do jiného package
+    @Scheduled(fixedRate = 60_000) // every minute
+    private void removeDocumentsWithExpiredTimestamp() {
+        Query query = Query.query(Criteria.where("expiration").exists(true));
+        List<Event> expiringEvents = mongoTemplate.find(query, Event.class);
+        System.out.println("expiring events count: " + expiringEvents.size());
+        for (Event event : expiringEvents) {
+            if (event.getExpiration() != null && event.getExpiration() <= (System.currentTimeMillis() / 1000L)) {
+                removeDocument(event.getId());
+            }
         }
+    }
+
+    private void removeDocument(String documentId) {
+        Query removeQuery = Query.query(Criteria.where("_id").is(documentId));
+        mongoTemplate.remove(removeQuery, "event");
     }
 }
