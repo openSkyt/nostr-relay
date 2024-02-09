af5216b26de7ed3d9cd424e4f559ac845dfab3a1
PavelSlimBilek <bilekpavel.p@gmail.com>
2024-02-09 09:17:26 +0100

    Service layer separated

--- src/main/java/org/openskyt/nostrrelay/controller/SubscriptionController.java
+++ src/main/java/org/openskyt/nostrrelay/controller/SubscriptionController.java
@@ -7,6 +7,7 @@ import org.openskyt.nostrrelay.model.Event;
 import org.openskyt.nostrrelay.model.NostrConsumer;
 import org.openskyt.nostrrelay.observers.EventObserver;
 import org.openskyt.nostrrelay.service.EventService;
+import org.openskyt.nostrrelay.service.SubscriptionManager;
 import org.openskyt.nostrrelay.service.SubscriptionService;
 import org.openskyt.nostrrelay.util.NostrUtil;
 import org.openskyt.nostrrelay.observers.CloseObserver;
@@ -21,21 +22,18 @@ import java.util.Set;
 @Component
 public class SubscriptionController implements NostrConsumer {
 
+    private final SubscriptionManager subscriptionManager;
     private final SubscriptionService subscriptionService;
-    private final EventService eventService;
-    private final NostrUtil util;
 
-    public SubscriptionController(SubscriptionService subscriptionService,
-                                  EventService eventService,
-                                  NostrUtil util,
+    public SubscriptionController(SubscriptionManager subscriptionManager,
+                                  SubscriptionService subscriptionService,
                                   ReqObserver reqObserver,
                                   CloseObserver closeObserver,
                                   SessionObserver sessionObserver,
                                   EventObserver eventObserver) {
 
+        this.subscriptionManager = subscriptionManager;
         this.subscriptionService = subscriptionService;
-        this.eventService = eventService;
-        this.util = util;
         reqObserver.subscribe(this);
         closeObserver.subscribe(this);
         sessionObserver.subscribe(this);
@@ -45,102 +43,17 @@ public class SubscriptionController implements NostrConsumer {
     @Override
     public void handle(Object o) {
         if (o instanceof Subscription) {
-            subscriptionService.createNewSubscription((Subscription) o);
-            sendPersistedData((Subscription) o);
+            subscriptionManager.createNewSubscription((Subscription) o);
+            subscriptionService.sendPersistedData((Subscription) o);
 
         } else if (o instanceof CloseData) {
-            subscriptionService.removeSubscription((CloseData) o);
+            subscriptionManager.removeSubscription((CloseData) o);
 
         } else if (o instanceof WebSocketSession) {
-            subscriptionService.removeSubscription((WebSocketSession) o);
+            subscriptionManager.removeSubscription((WebSocketSession) o);
 
         } else if (o instanceof  Event) {
-            handleNewEvent((Event) o);
+            subscriptionService.handleNewEvent((Event) o);
         }
-    }
-
-    /**
-     * Feeds newly created subscription with REQuested retrieved persisted EVENT-data
-     */
-    public void sendPersistedData(Subscription subscription) {
-        Set<Event> events = eventService.getAllEvents(subscription.filters());
-        sendEvents(subscription, events);
-        try {
-            subscription.session().sendMessage(util.eoseMessage(subscription));
-        } catch (IOException e) {
-            throw new RuntimeException(e);
-        }
-    }
-
-    /**
-     * Feeds current subscriptions with REQuested incoming EVENT-data
-     * @param event examined EVENT-data to filter
-     */
-    public void handleNewEvent(Event event) {
-        for (Subscription subscription : subscriptionService.getAllSubscriptions()) {
-            if (doesMatch(event, subscription.filters())) {
-                sendEvents(subscription, Set.of(event));
-            }
-        }
-    }
-
-    /**
-     * Compares EVENT-data to REQ-data specifics. Note there might be more REQ-data for a single subscription.
-     * @param event         EVENT-data to examine
-     * @param reqFilterSet  REQ-data SET to filter by
-     * @return true if the event matches any of the filters, false otherwise
-     */
-    private boolean doesMatch(Event event, Set<ReqFilter> reqFilterSet) {
-        if (reqFilterSet == null) {
-            return false;
-        }
-
-        for (ReqFilter r : reqFilterSet) {
-            if (
-                    (r.getKinds() == null || r.getKinds().isEmpty() || r.getKinds().contains(event.getKind()))
-                            && (r.getAuthors() == null || r.getAuthors().isEmpty() || r.getAuthors().contains(event.getPubkey()))
-                            && (r.getIds() == null || r.getIds().isEmpty() || r.getIds().contains(event.getId()))
-                            && (r.getSince() == null || r.getSince() <= event.getCreated_at())
-                            && (r.getUntil() == null || r.getUntil() >= event.getCreated_at())
-                            && (r.getE() == null || r.getE().isEmpty() || containsTags(event.getTags(), "e", r.getE()))
-                            && (r.getP() == null || r.getP().isEmpty() || containsTags(event.getTags(), "p", r.getP()))
-                            && (r.getT() == null || r.getT().isEmpty() || containsTags(event.getTags(), "t", r.getT()))) {
-                return true;
-            }
-        }
-        return false;
-    }
-
-    /**
-     * Checks if the given list of tags contains all tags with the specified key and values.
-     * @param tags     List of tags to check
-     * @param key      Tag key
-     * @param values   Tag values to match
-     * @return true if the list contains a tag with the specified key and values, false otherwise
-     */
-    private boolean containsTags(String[][] tags, String key, Set<String> values) {
-        if (tags == null || tags.length == 0 || key == null || key.isEmpty() || values == null || values.isEmpty()) {
-            return true; // If any of the parameters are null or empty, consider it a match
-        }
-        for (String[] tag : tags) {
-            if (tag.length >= 2 && !key.equals(tag[0]) && values.contains(tag[1])) {
-                return false;
-            }
-        }
-        return true;
-    }
-
-    /**
-     * Sends parsed EVENT-data SET to client (sub method)
-     *
-     */
-    private void sendEvents(Subscription subscription, Set<Event> events) {
-        events.forEach(event -> {
-            try {
-                subscription.session().sendMessage(util.stringifyEvent(event, subscription));
-            } catch (IOException e) {
-                throw new RuntimeException();
-            }
-        });
     }
 }
---
+++ src/main/java/org/openskyt/nostrrelay/service/SubscriptionManager.java
@@ -0,0 +1,37 @@
+package org.openskyt.nostrrelay.service;
+
+import org.openskyt.nostrrelay.dto.CloseData;
+import org.openskyt.nostrrelay.dto.Subscription;
+import org.springframework.stereotype.Component;
+import org.springframework.web.socket.WebSocketSession;
+
+import java.util.HashSet;
+import java.util.Set;
+
+@Component
+public class SubscriptionManager {
+
+    private final Set<Subscription> activeSubscriptions = new HashSet<>();
+
+    public Set<Subscription> getAllSubscriptions() {
+        return activeSubscriptions;
+    }
+
+    public void createNewSubscription(Subscription subscription) {
+        activeSubscriptions.add(subscription);
+    }
+
+    public void removeSubscription(CloseData closeData) {
+        for (Subscription s : activeSubscriptions) {
+            if (s.subscription_id().equals(closeData.subscription_id())
+                    && s.session().equals(closeData.session())) {
+                activeSubscriptions.remove(s);
+                return;
+            }
+        }
+    }
+
+    public void removeSubscription(WebSocketSession session) {
+        activeSubscriptions.removeIf(sub -> sub.session().equals(session));
+    }
+}
